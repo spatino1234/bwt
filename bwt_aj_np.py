33863289dbe354dcaae0a31016e51469bf109881
AJ <ristinoa@carleton.edu>
2023-10-01 21:29:46 -0500

    Annotated code

--- bwt_aj_np.py
+++ bwt_aj_np.py
@@ -8,13 +8,20 @@ input = input + '$'
 
 indices = np.arange(len(input))
 
-# Lots of cool stuff to talk about here:
+# Permute and lexicographically sort string permutations
 sorted_indices = sorted(indices, key=lambda i: input[i:] + input[:i])
-print(sorted_indices)
-# One-line key function both creates, then sorts lexicographically, the permutations
+# The key function takes an index i and constructs a string by concatenating the substring 
+# of input from index i to the end (input[i:]) with the substring from the beginning to 
+# index i (input[:i]). This effectively sorts the indices based on their corresponding cyclic permutations of the input string.
+
+# Short version: The output is a list of indexes ordered lexicographically, this is used in the following step:
 
 # use sorted indices to map final characters of sorted permutations to a variable
 bwt_encoded = ''.join(input[i - 1] for i in sorted_indices)
+# Above list comprehension takes an index in sorted_indices and joins input[i - 1] into the encoded string
+# This works because the bwt encoding phase is a series of rightward permutations
+# This means that in the original string, if i is your first sorted index, i-1 is the final character in the permuted string
+# Running through the entire list using the inline for loop builds the codeword based off the sorted indices
 
 print(f"Your BWT encoded codeword is: {bwt_encoded}")
 
