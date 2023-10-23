269d41393af1f25fba65260e9b9b60bf6094f151
AJ <ristinoa@carleton.edu>
2023-10-23 18:02:28 -0500

    Edits**

--- GUI/gui.py
+++ GUI/gui.py
@@ -6,18 +6,6 @@ def draw_text(text, x, y):
     t.pendown()
     t.write(text, font=("Arial", 16, "normal"))
 
-def draw_arrow(x1, y1, x2, y2):
-    t.penup()
-    t.goto(x1, y1)
-    t.pendown()
-    t.setheading(t.towards(x2, y2))
-    t.forward(t.distance(x2, y2))
-    t.penup()
-    t.goto(x2, y2)
-    t.pendown()
-    t.setheading(t.towards(x1, y1))
-    t.forward(t.distance(x1, y1))
-
 # Initialize the turtle
 wn = turtle.Screen()
 t = turtle.Turtle()
@@ -25,7 +13,7 @@ t.speed(1)
 t.hideturtle()
 
 # BWT input string
-input_string = "ACATGAGCAT"
+input_string = turtle.textinput("Input", "Enter a string:")
 
 # Perform BWT
 input_string += '$'
@@ -39,8 +27,6 @@ draw_text(input_string, -100, 70)
 
 draw_text("BWT Encoded String:", -200, 0)
 draw_text(bwt_encoded, -100, -30)
-
-# draw_arrow(-100, 50, -100, 10)
 
 # Close the turtle graphics window on click
 wn.exitonclick()
