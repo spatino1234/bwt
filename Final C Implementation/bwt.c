5b35b2a8c43a3cd2d6a75329cb141e835fb82c94
Jack Owens <jackowens@Jacks-Macbook-Pro-2.local>
2023-11-12 11:18:53 -0600

    Addded Radix to bwt.c

--- Final C Implementation/bwt.c
+++ Final C Implementation/bwt.c
@@ -398,72 +398,366 @@ void freeBW(BurrowsWheeler *BW) {
     free(BW);
 }
 
-int main()  {
-    // Getting user input
-    char genomeFile[100];
-    printf("\nEnter the name of the .txt file with your reference genome: ");
-    scanf("%100s",genomeFile);
-    
-    // Reading genome from txt file input by user
-    char *genome = readGenome(genomeFile);
-    int genome_length = strlen(genome);
-    if (genome_length > 100001) {
-        printf("\nERROR: %s is too long. It can be at most 100,000 characters\n\n", genomeFile);
-        exit(0);
-    }
-    
-    // Encoding genome using BWT
-    transform(genome, genome_length);
-    BurrowsWheeler *BW = initBW(genome_length+1);
-    constructIndices(BW, genome);
 
-    // 
-    printf("\n");
-    while (1) {
-        char query1[300];
-        printf("\n\e[4mEnter a Query\e[m \nTo exit, \t  enter the query \"exit\"\nFor more options, enter the query \"help\"\n\n\e[1mQuery\e[m: ");
-        scanf("%300s", query1);
-        if (strncmp(query1, "exit", 4) == 0) {
-            printf("\n");
-            exit(0);
-        }
-        else if (strncmp(query1, "fastq", 5) == 0) {
-            char fastqFile[100];
-            printf("\tEnter .fastq file name: ");
-            scanf("%100s",fastqFile);
-            printf("\n\n");
-            queryFASTQ(BW, fastqFile);
-            fflush(stdin);
-            continue;
-        }
-        else if (strncmp(query1, "match", 5) == 0) {
-            char query2[300];
-            printf("\e[1mActual Query:\e[m ");
-            scanf("%300s",query2);
+/* wraps array index within array bounds (assumes value < 2 * limit) */
+#define Wrap(value, limit)      (((value) < (limit)) ? (value) : ((value) - (limit)))
 
-            if (strlen(query2) < 251) {
-                query(BW, query2, 0, 1);
-            } else {
-                printf("\nERROR: Query is too long. It must be at most 250 characters\n\n");
-                fflush(stdin);
-            }
+int *block;     /* block being (un)transformed */
+size_t blockSize;                    /* actual size of block */
+
+static int ComparePresorted(const void *s1, const void *s2)
+{
+    int offset1, offset2;
+    int i;
+
+    /***********************************************************************
+    * Compare 1 character at a time until there's difference or the end of
+    * the block is reached.  Since we're only sorting strings that already
+    * match at the first two characters, start with the third character.
+    ***********************************************************************/
+    offset1 = *((int *)s1) + 2;
+    offset2 = *((int *)s2) + 2;
+
+    for(i = 2; i < blockSize; i++)
+    {
+        int c1, c2;
+
+        /* ensure that offsets are properly bounded */
+        if (offset1 >= blockSize)
+        {
+            offset1 -= blockSize;
         }
-        else if (strncmp(query1, "help", 5) == 0) {
-            printf("\n * To read queries from a .fastq file, enter the query \"fastq\"  *\n");
-            printf(" * To output the locations of matches, enter the query \"match\"  *\n");
+
+        if (offset2 >= blockSize)
+        {
+            offset2 -= blockSize;
         }
-        else {
-            if (strlen(query1) < 251) {
-                query(BW, query1, 0, 0);
-                fflush(stdin);
-            } else {
-                printf("\nERROR: Query is too long. It must be at most 250 characters\n\n");
-                fflush(stdin);
-            }
+
+        c1 = block[offset1];
+        c2 = block[offset2];
+
+        if (c1 > c2)
+        {
+            return 1;
         }
-        printf("\n");
+        else if (c2 > c1)
+        {
+            return -1;
+        }
+
+        /* strings match to here, try next character */
+        offset1++;
+        offset2++;
     }
 
-    freeBW(BW);
+    /* strings are identical */
     return 0;
 }
+
+static int ComparePresortedFull(const void *s1, const void *s2)
+{
+    int offset1, offset2;
+    int i;
+
+    /***********************************************************************
+    * Compare 1 character at a time until there's difference or the end of
+    * the block is reached.  This time start with the first character.
+    ***********************************************************************/
+    offset1 = *((int *)s1) + 2;
+    offset2 = *((int *)s2) + 2;
+
+    for(i = 0; i < blockSize; i++)
+    {
+        int c1, c2;
+
+        /* ensure that offsets are properly bounded */
+        if (offset1 >= blockSize)
+        {
+            offset1 -= blockSize;
+        }
+
+        if (offset2 >= blockSize)
+        {
+            offset2 -= blockSize;
+        }
+
+        c1 = block[offset1];
+        c2 = block[offset2];
+
+        if (c1 > c2)
+        {
+            return 1;
+        }
+        else if (c2 > c1)
+        {
+            return -1;
+        }
+
+        /* strings match to here, try next character */
+        offset1++;
+        offset2++;
+    }
+
+    /* strings are identical */
+    return 0;
+}
+
+void radix_bwt(int *last)
+{
+    int i, j, k;
+    int *rotationIdx;      /* index of first char in rotation */
+    int *v;                /* index of radix sorted charaters */
+    int s0Idx;                      /* index of S0 in rotations (I) */
+    // int *last;            /* last characters from sorted rotations */
+
+    /* counters and offsets used for radix sorting with characters */
+    unsigned int counters[5] = {0};
+    unsigned int offsetTable[5] = {0};
+
+    /***********************************************************************
+    * BLOCK_SIZE arrays are allocated on the heap, because gcc generates
+    * code that throws a Segmentation fault when the large arrays are
+    * allocated on the stack.
+    ***********************************************************************/
+    rotationIdx = (int *)malloc(blockSize * sizeof(int));
+
+    v = (int *)malloc(blockSize * sizeof(int));
+
+    /*******************************************************************
+    * Sort the rotated strings in the block.  A radix sort is performed
+    * on the first to characters of all the rotated strings (2nd
+    * character then 1st).  All rotated strings with matching initial
+    * characters are then quicksorted. - Q4..Q7
+    *******************************************************************/
+
+    /*** radix sort on second character in rotation ***/
+
+    /* count number of characters for radix sort */
+    for (i = 0; i < blockSize; i++)
+    {
+        counters[block[i]]++;
+    }
+
+    offsetTable[0] = 0;
+
+    for(i = 1; i < 5; i++)
+    {
+        /* determine number of values before those sorted under i */
+        offsetTable[i] = offsetTable[i - 1] + counters[i - 1];
+    }
+
+    /* sort on 2nd character */
+    for (i = 0; i < blockSize - 1; i++)
+    {
+        j = block[i + 1];
+        v[offsetTable[j]] = i;
+        offsetTable[j] = offsetTable[j] + 1;
+    }
+
+    /* handle wrap around for string starting at end of block */
+    j = block[0];
+    v[offsetTable[j]] = i;
+    offsetTable[0] = 0;
+
+    /*** radix sort on first character in rotation ***/
+
+    for(i = 1; i < 5; i++)
+    {
+        /* determine number of values before those sorted under i */
+        offsetTable[i] = offsetTable[i - 1] + counters[i - 1];
+    }
+
+    for (i = 0; i < blockSize; i++)
+    {
+        j = v[i];
+        j = block[j];
+        rotationIdx[offsetTable[j]] = v[i];
+        offsetTable[j] = offsetTable[j] + 1;
+    }
+
+    /*******************************************************************
+    * now rotationIdx contains the sort order of all strings sorted
+    * by their first 2 characters.  Use qsort to sort the strings
+    * that have their first two characters matching.
+    *******************************************************************/
+    for (i = 0, k = 0; (i < 5 && k < (blockSize - 1)); i++)
+    {
+        for (j = 0; (j < 5 && k < (blockSize - 1)); j++)
+        {
+            unsigned int first = k;
+
+            /* count strings starting with ij */
+            while ((i == block[rotationIdx[k]]) &&
+                (j == block[Wrap(rotationIdx[k] + 1,  blockSize)]))
+            {
+                k++;
+
+                if (k == blockSize)
+                {
+                    /* we've searched the whole block */
+                    break;
+                }
+            }
+
+            if (k - first > 1)
+            {
+                /* there are at least 2 strings staring with ij, sort them */
+                qsort(&rotationIdx[first], k - first, sizeof(int),
+                    ComparePresorted);
+            }
+        }
+    }
+
+    /* find last characters of rotations (L) - C2 */
+    s0Idx = 0;
+    for (i = 0; i < blockSize; i++)
+    {
+        if (rotationIdx[i] != 0)
+        {
+            last[i] = block[rotationIdx[i] - 1];
+        }
+        else
+        {
+            /* unrotated string 1st character is end of string */
+            s0Idx = i;
+            last[i] = block[blockSize - 1];
+        }
+    }
+
+    /* clean up */
+    free(rotationIdx);
+    free(v);
+    return;
+}
+
+void encodeString(char *input) {
+    int input_len = strlen(input);
+    int encodedCharacter;
+    for (int i = 0; i < input_len; i++) {
+        encodedCharacter = encodeCharacter(input[i]);
+        block[i] = encodedCharacter;
+    }
+    return;
+}
+
+void decodeList(char laststr[], int *input) {
+    char decoded_string[blockSize];
+    strcpy (decoded_string,"");
+    int cur;
+    char character;
+    for (int i = 0; i < blockSize; i++) {
+        cur = input[i];
+        character = decodeCharacter(cur);
+        strncat(decoded_string, &character, 1);
+    }
+    strcpy(laststr, decoded_string);
+    return;
+}
+
+void BWTRadix(char *blockstr, char *transformed_string) {
+    blockSize = strlen(blockstr);
+    block = (int*) malloc(sizeof(int)*blockSize);
+    encodeString(blockstr);
+    int *last;
+    last = (int *)malloc(blockSize * sizeof(int));
+    radix_bwt(last);
+    decodeList(transformed_string, last);
+    free(last);
+    free(block);
+    return;
+}
+
+void genBWTString(int length, char *bwt_string) {
+    int i;
+    int bwt_intstring[length];
+    for (i = 0; i < length; i++) { 
+        int num = rand() % 5; 
+        bwt_intstring[i] = num;
+    }
+    blockSize = length;
+    decodeList(bwt_string, bwt_intstring);
+    return;
+}
+
+int main(){
+    char *genome = "GATATA$";
+
+    char transform[strlen(genome)];
+    BWTRadix(genome, transform);
+    printf("%s\n",transform);
+
+    return 0;
+}
+
+
+
+
+// int main()  {
+//     // Getting user input
+//     char genomeFile[100];
+//     printf("\nEnter the name of the .txt file with your reference genome: ");
+//     scanf("%100s",genomeFile);
+    
+//     // Reading genome from txt file input by user
+//     char *genome = readGenome(genomeFile);
+//     int genome_length = strlen(genome);
+//     if (genome_length > 100001) {
+//         printf("\nERROR: %s is too long. It can be at most 100,000 characters\n\n", genomeFile);
+//         exit(0);
+//     }
+    
+//     // Encoding genome using BWT
+//     transform(genome, genome_length);
+//     BurrowsWheeler *BW = initBW(genome_length+1);
+//     constructIndices(BW, genome);
+
+//     // 
+//     printf("\n");
+//     while (1) {
+//         char query1[300];
+//         printf("\n\e[4mEnter a Query\e[m \nTo exit, \t  enter the query \"exit\"\nFor more options, enter the query \"help\"\n\n\e[1mQuery\e[m: ");
+//         scanf("%300s", query1);
+//         if (strncmp(query1, "exit", 4) == 0) {
+//             printf("\n");
+//             exit(0);
+//         }
+//         else if (strncmp(query1, "fastq", 5) == 0) {
+//             char fastqFile[100];
+//             printf("\tEnter .fastq file name: ");
+//             scanf("%100s",fastqFile);
+//             printf("\n\n");
+//             queryFASTQ(BW, fastqFile);
+//             fflush(stdin);
+//             continue;
+//         }
+//         else if (strncmp(query1, "match", 5) == 0) {
+//             char query2[300];
+//             printf("\e[1mActual Query:\e[m ");
+//             scanf("%300s",query2);
+
+//             if (strlen(query2) < 251) {
+//                 query(BW, query2, 0, 1);
+//             } else {
+//                 printf("\nERROR: Query is too long. It must be at most 250 characters\n\n");
+//                 fflush(stdin);
+//             }
+//         }
+//         else if (strncmp(query1, "help", 5) == 0) {
+//             printf("\n * To read queries from a .fastq file, enter the query \"fastq\"  *\n");
+//             printf(" * To output the locations of matches, enter the query \"match\"  *\n");
+//         }
+//         else {
+//             if (strlen(query1) < 251) {
+//                 query(BW, query1, 0, 0);
+//                 fflush(stdin);
+//             } else {
+//                 printf("\nERROR: Query is too long. It must be at most 250 characters\n\n");
+//                 fflush(stdin);
+//             }
+//         }
+//         printf("\n");
+//     }
+
+//     freeBW(BW);
+//     return 0;
+// }
